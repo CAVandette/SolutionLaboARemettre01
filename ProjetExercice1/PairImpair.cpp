d710e33445f1e47003eeca3b2ac6a6ef7a4a9d16
Charles-Auguste Vandette <2077700@cstj.qc.ca>
2021-09-02 00:53:45 -0400

    Ajoutez des fichiers projet.

---
+++ ProjetExercice1/PairImpair.cpp
@@ -0,0 +1,39 @@
+// But : Écrire un programme qui lit un entier et qui détermine et affiche 
+// s’il s’agit d’un entier pair ou d’un entier impair.
+// Auteur : Charles-Auguste Vandette
+// Date : 2021-09-01
+
+#include<iostream>
+
+void main() // Fonction principale (Premier bloc d'instruction)
+{
+	setlocale(LC_ALL, "");
+
+	int nombre1; // Initialisation d'une variable de type int (entier)
+
+	std::cout << "Veuillez entrer une nombre entier pour tester la parité : ";
+	std::cin >> nombre1;
+	// On demande à l'utilisateur d'entrer un nombre entier
+
+	std::cout << std::endl;
+	// On saute une ligne
+
+	if (nombre1 % 2 == 0) // Si le résultat de nombre1 mod 2 est égale à 0, le nomlbre est pair
+	{
+		std::cout << "Ce nombre est pair" << std::endl;
+	}
+	else // Sinon, il est impair
+	{
+		std::cout << "Ce nombre est impair" << std::endl;
+	}
+}
+
+//			Plan de test V.1
+//		Essaie		Donnée		Résultat
+//		no1			0			"Ce nombre est pair"
+//		no2			1			"Ce nombre est impair"
+//		no3			-1			"Ce nombre est impair"
+//		no4			10			"Ce nombre est pair"
+//		no5			11			"Ce nombre est impair"
+//		no6			125			"Ce nombre est impair"
+//		no7			-150		"Ce nombre est pair"
---
+++ ProjetExercice2/Vertical.cpp
@@ -0,0 +1,45 @@
+// But : Écrire un programme qui prend comme entrée un nombre de cinq chiffres. 
+// Le programme vérifie qu'il y a bien 5 chiffres dans le nombre. Puis il affiche 
+// le nombre verticalement
+// Auteur : Charles-Auguste Vandette
+// Date : 2021-09-01
+
+#include<iostream>
+
+void main() // Fonction principale (premier bloc d'instruction)
+{
+	setlocale(LC_ALL, "");
+
+	int nombre1; // Initialisation de la variable à tester
+
+	std::cout << "Veuillez entrer un entier à 5 chiffres : ";
+	std::cin >> nombre1;
+	// On demande à l'utilisateur d'entrer un nombre et il l'écrit
+
+	if (nombre1 < 10000 || nombre1 > 99999) // On s'assure que le chiffre possède 5 chiffres et non 4 ou 6
+	{
+		std::cout << "Erreur, ce nombre ne contient pas 5 chiffres!";
+		// Dans le cas d'une erreur, on l'indique à l'utilisateur
+	}
+	else // sinon : on affiche le résultat attendu
+	{
+		std::cout << (nombre1 / 10000) << std::endl; // Division pour obtenir le premier chiffre
+		std::cout << (nombre1 / 1000) % 10 << std::endl; // Division + mod pour obtenir le second chiffre
+		std::cout << (nombre1 / 100) % 10 << std::endl;
+		std::cout << (nombre1 / 10) % 10 << std::endl;
+		std::cout << (nombre1 % 10) << std::endl; // Mod pour obtenir le dernier chiffre
+	}
+}
+
+//			Plan de test V.1
+//		Essaie		Donnée		Résultat
+//		no1			1234		"Erreur, ce nombre ne contient pas 5 chiffres!"
+//		no2			123456		"Erreur, ce nombre ne contient pas 5 chiffres!"
+//		no3			-1234		"Erreur, ce nombre ne contient pas 5 chiffres!"
+//		no4			-123456		"Erreur, ce nombre ne contient pas 5 chiffres!"
+//		no5			-12345		"Erreur, ce nombre ne contient pas 5 chiffres!"
+//		no6			12345		1
+//								2
+//								3
+//								4
+//								5
